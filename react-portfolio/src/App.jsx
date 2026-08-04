4d7742696308b693cb68300489ff9f429c957bb4
Tasuntha Chathunika <tasunthachathunika@gmail.com>
2026-08-05 01:22:30 +0530

    Clean up unused components and update App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import TechMarquee from './components/TechMarquee';
 import About from './components/About';
 import Education from './components/Education';
 import Skills from './components/Skills';
@@ -68,7 +67,6 @@ function App() {
       <Navbar />
       <main className="relative z-10">
         <Hero />
-        <TechMarquee />
         <SectionDivider />
         <About />
         <SectionDivider flip />
